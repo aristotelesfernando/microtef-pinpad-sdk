549230b10cb4865057e59a355b1a61643badd692
Ceres <ccarvalho@stone.com.br>
2016-03-28 11:59:07 -0300

    Retirada de testes fake.

--- Pinpad.Core.Rebuild.Test/UnitTest1.cs
+++ Pinpad.Core.Rebuild.Test/UnitTest1.cs
@@ -9,16 +9,7 @@ namespace Pinpad.Core.Rebuild.Test
 		[TestMethod]
 		public void TestMethod1 ()
 		{
-			IPinpadResponse resp1 = new GertecResponse();
-			Assert.AreEqual(resp1.ResponseCode, GertecResponseCode.Xpto2);
-
-			if ((GertecResponseCode) resp1.ResponseCode == GertecResponseCode.Xpto2)
-			{
-
-			}
-
-			resp1 = new AbecsResponse();
-			Assert.AreEqual(resp1.ResponseCode, AbecsResponseCode.Abcd1);
+			
 		}
 	}
 }
