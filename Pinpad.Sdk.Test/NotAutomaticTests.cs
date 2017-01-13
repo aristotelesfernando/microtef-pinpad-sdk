c9996e5ff9427f2bd47dbf2510251bf6118f1879
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-01-13 17:39:41 -0200

    :green_heart: Atualiza versão para não dar ruim no cache do CI (again).

--- Pinpad.Sdk.Test/NotAutomaticTests.cs
+++ Pinpad.Sdk.Test/NotAutomaticTests.cs
@@ -16,10 +16,10 @@ namespace Pinpad.Sdk.Test
     [TestClass]
 	public class NotAutomaticTests
 	{
-		[TestInitialize]
+		//[TestInitialize]
 		public void Setup ()
 		{
-			MicroPos.Platform.Desktop.DesktopInitializer.Initialize();
+			//MicroPos.Platform.Desktop.DesktopInitializer.Initialize();
 		}
 
 		//[TestMethod]
@@ -221,7 +221,7 @@ namespace Pinpad.Sdk.Test
 
 			Debug.WriteLine(value);
 		}
-		[TestMethod]
+		//[TestMethod]
 		public void PinpadTransaction_ReadCard_test ()
 		{
             PinpadConnection conn = PinpadConnection.GetFirst();
