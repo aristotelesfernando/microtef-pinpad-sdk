5053392842a2f56a4824fa530ea19328c4824623
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-05-19 17:33:52 -0300

    MCRTEFSDK-97 :rocket: Add UPE.

---
+++ Pinpad.Sdk/Commands/Request/UpeRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pinpad.Sdk.Commands.Request
+{
+    // TODO: Doc
+    public sealed class UpeRequest : BaseCommand
+    {
+        public override string CommandName { get { return "UPE"; } }
+
+        public UpeRequest() { }
+    }
+}
