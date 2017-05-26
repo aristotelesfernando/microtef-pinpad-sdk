ac4a021b4c163585f8878cf34b300adcb5efba14
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-05-26 17:04:33 -0300

    MCRTEFSDK-97 :rocket: Porta o TLE na nova arquitetura.

--- Pinpad.Sdk/Commands/Request/TleRequest.cs
+++ Pinpad.Sdk/Commands/Request/TleRequest.cs
@@ -3,16 +3,16 @@
     /// <summary>
     /// TLE request
     /// </summary>
-    internal sealed class TleRequest : BaseCommand
+    internal sealed class TleRequest : PinpadProperties.Refactor.BaseCommand
     {
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public TleRequest() { }
-
         /// <summary>
         /// Name of the command
         /// </summary>
         public override string CommandName { get { return "TLE"; } }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public TleRequest() { }
     }
 }
