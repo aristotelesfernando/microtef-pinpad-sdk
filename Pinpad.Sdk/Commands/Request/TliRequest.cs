d9d8d1577f5377205815a0839bc1147f7c22d6a9
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-05-26 17:05:45 -0300

    MCRTEFSDK-97 :rocket: Porta o TLI na nova arquitetura.

--- Pinpad.Sdk/Commands/Request/TliRequest.cs
+++ Pinpad.Sdk/Commands/Request/TliRequest.cs
@@ -1,4 +1,6 @@
-﻿using Pinpad.Sdk.Properties;
+﻿using Pinpad.Sdk.PinpadProperties.Refactor.Formatter;
+using Pinpad.Sdk.PinpadProperties.Refactor.Parser;
+using Pinpad.Sdk.PinpadProperties.Refactor.Property;
 using System;
 
 namespace Pinpad.Sdk.Commands
@@ -6,17 +8,37 @@ namespace Pinpad.Sdk.Commands
 	/// <summary>
 	/// TLI request
 	/// </summary>
-	internal sealed class TliRequest : BaseCommand
+	internal sealed class TliRequest : PinpadProperties.Refactor.BaseCommand
 	{
-		/// <summary>
-		/// Constructor
+        /// <summary>
+		/// Name of the command
 		/// </summary>
-		public TliRequest() 
+		public override string CommandName { get { return "TLI"; } }
+        /// <summary>
+        /// Length of the first region of the command
+        /// </summary>
+        public RegionProperty CMD_LEN1 { get; private set; }
+        /// <summary>
+        /// EMV Table acquirer index to update
+        /// To change all acquirers use the generic index (0)
+        /// </summary>
+        public FixedLengthProperty<Nullable<int>> TLI_ACQIDX { get; private set; }
+        /// <summary>
+        /// EMV Table version to load
+        /// </summary>
+        public FixedLengthProperty<string> TLI_TABVER { get; private set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public TliRequest() 
 			: base (new AbecsContext())
 		{
 			this.CMD_LEN1 = new RegionProperty("CMD_LEN1", 3);
-			this.TLI_ACQIDX = new PinpadFixedLengthProperty<Nullable<int>>("TLI_ACQIDX", 2, false, DefaultStringFormatter.IntegerStringFormatter, DefaultStringParser.IntegerStringParser);
-			this.TLI_TABVER = new PinpadFixedLengthProperty<string>("TLI_TABVER", 10, false, DefaultStringFormatter.StringStringFormatter, DefaultStringParser.StringStringParser);
+			this.TLI_ACQIDX = new FixedLengthProperty<Nullable<int>>("TLI_ACQIDX", 2, false, 
+                StringFormatter.IntegerStringFormatter, StringParser.IntegerStringParser);
+			this.TLI_TABVER = new FixedLengthProperty<string>("TLI_TABVER", 10, false, 
+                StringFormatter.StringStringFormatter, StringParser.StringStringParser);
 
 			this.StartRegion(this.CMD_LEN1);
 			{
@@ -25,26 +47,5 @@ namespace Pinpad.Sdk.Commands
 			}
 			this.EndLastRegion();
 		}
-
-		/// <summary>
-		/// Name of the command
-		/// </summary>
-		public override string CommandName { get { return "TLI"; } }
-
-		/// <summary>
-		/// Length of the first region of the command
-		/// </summary>
-		public RegionProperty CMD_LEN1 { get; private set; }
-
-		/// <summary>
-		/// EMV Table acquirer index to update
-		/// To change all acquirers use the generic index (0)
-		/// </summary>
-		public PinpadFixedLengthProperty<Nullable<int>> TLI_ACQIDX { get; private set; }
-
-		/// <summary>
-		/// EMV Table version to load
-		/// </summary>
-		public PinpadFixedLengthProperty<string> TLI_TABVER { get; private set; }
 	}
 }
