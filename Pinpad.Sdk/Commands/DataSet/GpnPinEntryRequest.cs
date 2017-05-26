ac01986db4e806b07b1b1eb177ad7e415efd994b
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-05-26 16:56:05 -0300

    MCRTEFSDK-97 :rocket: Porta o GPN e PinpadInfos na nova arquitetura.

--- Pinpad.Sdk/Commands/DataSet/GpnPinEntryRequest.cs
+++ Pinpad.Sdk/Commands/DataSet/GpnPinEntryRequest.cs
@@ -1,4 +1,6 @@
-﻿using Pinpad.Sdk.Properties;
+﻿using Pinpad.Sdk.PinpadProperties.Refactor.Formatter;
+using Pinpad.Sdk.PinpadProperties.Refactor.Parser;
+using Pinpad.Sdk.PinpadProperties.Refactor.Property;
 using System;
 
 namespace Pinpad.Sdk.Commands
@@ -6,35 +8,37 @@ namespace Pinpad.Sdk.Commands
 	/// <summary>
 	/// GPN pin entry record
 	/// </summary>
-	public sealed class GpnPinEntryRequest : BaseProperty 
+	public sealed class GpnPinEntryRequest : PinpadProperties.Refactor.Property.BaseProperty 
 	{
-		/// <summary>
-		/// Constructor
+        /// <summary>
+		/// Minimum pin digits
 		/// </summary>
-		public GpnPinEntryRequest()
+		public FixedLengthProperty<Nullable<int>> GPN_MIN { get; private set; }
+        /// <summary>
+        /// Maximum pin digits
+        /// </summary>
+        public FixedLengthProperty<Nullable<int>> GPN_MAX { get; private set; }
+        /// <summary>
+        /// Pin Capture Message
+        /// </summary>
+        public TextProperty<SimpleMessageProperty> GPN_MSG { get; private set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GpnPinEntryRequest()
 		{
-			this.GPN_MIN = new PinpadFixedLengthProperty<Nullable<int>>("GPN_MIN", 2, false, DefaultStringFormatter.IntegerStringFormatter, DefaultStringParser.IntegerStringParser);
-			this.GPN_MAX = new PinpadFixedLengthProperty<Nullable<int>>("GPN_MAX", 2, false, DefaultStringFormatter.IntegerStringFormatter, DefaultStringParser.IntegerStringParser);
-			this.GPN_MSG = new SimpleProperty<SimpleMessage>("GPN_MSG", false, DefaultStringFormatter.PropertyControllerStringFormatter, SimpleMessage.StringParser, null, new SimpleMessage());
+			this.GPN_MIN = new FixedLengthProperty<Nullable<int>>("GPN_MIN", 2, false,
+                StringFormatter.IntegerStringFormatter, StringParser.IntegerStringParser);
+			this.GPN_MAX = new FixedLengthProperty<Nullable<int>>("GPN_MAX", 2, false,
+                StringFormatter.IntegerStringFormatter, StringParser.IntegerStringParser);
+			this.GPN_MSG = new TextProperty<SimpleMessageProperty>("GPN_MSG", false, 
+                StringFormatter.PropertyControllerStringFormatter, SimpleMessageProperty.CustomStringParser, 
+                null, new SimpleMessageProperty());
 
 			this.AddProperty(this.GPN_MIN);
 			this.AddProperty(this.GPN_MAX);
 			this.AddProperty(this.GPN_MSG);
 		}
-
-		/// <summary>
-		/// Minimum pin digits
-		/// </summary>
-		public PinpadFixedLengthProperty<Nullable<int>> GPN_MIN { get; private set; }
-
-		/// <summary>
-		/// Maximum pin digits
-		/// </summary>
-		public PinpadFixedLengthProperty<Nullable<int>> GPN_MAX { get; private set; }
-
-		/// <summary>
-		/// Pin Capture Message
-		/// </summary>
-		public SimpleProperty<SimpleMessage> GPN_MSG { get; private set; }
 	}
 }
