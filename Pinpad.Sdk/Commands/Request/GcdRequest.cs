af2ce285ed73b119c73aeb9c8cba35dfda3e60b8
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-05-26 14:38:35 -0300

    MCRTEFSDK-97 :rocket: Porta o GCD na nova arquitetura.

--- Pinpad.Sdk/Commands/Request/GcdRequest.cs
+++ Pinpad.Sdk/Commands/Request/GcdRequest.cs
@@ -1,10 +1,12 @@
 ﻿using Pinpad.Sdk.Model;
-using Pinpad.Sdk.Properties;
+using Pinpad.Sdk.PinpadProperties.Refactor.Formatter;
+using Pinpad.Sdk.PinpadProperties.Refactor.Parser;
+using Pinpad.Sdk.PinpadProperties.Refactor.Property;
 using System;
 
 namespace Pinpad.Sdk.Commands
 {
-	internal sealed class GcdRequest : BaseCommand
+	internal sealed class GcdRequest : PinpadProperties.Refactor.BaseCommand
 	{
 		public override string CommandName
 		{
@@ -12,18 +14,20 @@ namespace Pinpad.Sdk.Commands
 		}
 
 		public RegionProperty CMD_LEN1 { get; private set; }
-		public PinpadFixedLengthProperty<KeyboardMessageCode> SPE_MSGIDX { get; private set; }
-		public PinpadFixedLengthProperty<Nullable<int>> SPE_MINDIG { get; private set; }
-		public PinpadFixedLengthProperty<Nullable<int>> SPE_MAXDIG { get; private set; }
+		public FixedLengthProperty<KeyboardMessageCode> SPE_MSGIDX { get; private set; }
+		public FixedLengthProperty<Nullable<int>> SPE_MINDIG { get; private set; }
+		public FixedLengthProperty<Nullable<int>> SPE_MAXDIG { get; private set; }
 
 		public GcdRequest()
 		{
 			this.CMD_LEN1 = new RegionProperty("CMD_LEN1", 3, false, true);
-			this.SPE_MSGIDX = new PinpadFixedLengthProperty<KeyboardMessageCode>("SPE_MSGIDX", 4, false, DefaultStringFormatter.EnumStringFormatter<KeyboardMessageCode>, DefaultStringParser.EnumStringParser<KeyboardMessageCode>);
-
-			this.SPE_MINDIG = new PinpadFixedLengthProperty<Nullable<int>>("SPE_MINDIG", 2, false, DefaultStringFormatter.IntegerStringFormatter, DefaultStringParser.IntegerStringParser);
-
-			this.SPE_MAXDIG = new PinpadFixedLengthProperty<Nullable<int>>("SPE_MAXDIG", 2, false, DefaultStringFormatter.IntegerStringFormatter, DefaultStringParser.IntegerStringParser);
+			this.SPE_MSGIDX = new FixedLengthProperty<KeyboardMessageCode>("SPE_MSGIDX", 4, false, 
+                StringFormatter.EnumStringFormatter<KeyboardMessageCode>, 
+                StringParser.EnumStringParser<KeyboardMessageCode>);
+			this.SPE_MINDIG = new FixedLengthProperty<Nullable<int>>("SPE_MINDIG", 2, false, 
+                StringFormatter.IntegerStringFormatter, StringParser.IntegerStringParser);
+			this.SPE_MAXDIG = new FixedLengthProperty<Nullable<int>>("SPE_MAXDIG", 2, false, 
+                StringFormatter.IntegerStringFormatter, StringParser.IntegerStringParser);
 
 			this.AddProperty(this.SPE_MSGIDX);
 			this.AddProperty(this.SPE_MINDIG);
