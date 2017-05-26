bcc8c72bd6147386f6e0a40f7206e32be02cd478
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-05-26 16:48:21 -0300

    MCRTEFSDK-97 :rocket: Porta o GKY e PinpadInfos na nova arquitetura.

--- Pinpad.Sdk/Commands/Request/GkyRequest.cs
+++ Pinpad.Sdk/Commands/Request/GkyRequest.cs
@@ -3,16 +3,16 @@
     /// <summary>
     /// GKY request
     /// </summary>
-    internal sealed class GkyRequest : BaseCommand
+    internal sealed class GkyRequest : PinpadProperties.Refactor.BaseCommand
     {
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public GkyRequest() {  }
-
         /// <summary>
         /// Name of the command
         /// </summary>
         public override string CommandName { get { return "GKY"; } }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GkyRequest() {  }
     }
 }
