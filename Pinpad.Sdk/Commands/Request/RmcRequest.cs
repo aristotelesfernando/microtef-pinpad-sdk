c2002e6e629e6c61fa65f57600a01d0ab8e678c5
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-05-26 17:03:57 -0300

    MCRTEFSDK-97 :rocket: Porta o RMC na nova arquitetura.

--- Pinpad.Sdk/Commands/Request/RmcRequest.cs
+++ Pinpad.Sdk/Commands/Request/RmcRequest.cs
@@ -1,19 +1,36 @@
-﻿using Pinpad.Sdk.Properties;
+﻿using Pinpad.Sdk.PinpadProperties.Refactor.Formatter;
+using Pinpad.Sdk.PinpadProperties.Refactor.Property;
 
 namespace Pinpad.Sdk.Commands 
 {
 	/// <summary>
 	/// Remove Card Request
 	/// </summary>
-	internal sealed class RmcRequest : BaseCommand 
+	internal sealed class RmcRequest : PinpadProperties.Refactor.BaseCommand 
 	{
-		/// <summary>
-		/// Constructor
+        /// <summary>
+		/// Name of the command
 		/// </summary>
-		public RmcRequest() 
+		public override string CommandName { get { return "RMC"; } }
+        /// <summary>
+        /// Length of the first region of the command
+        /// </summary>
+        public RegionProperty CMD_LEN1 { get; private set; }
+        /// <summary>
+        /// Message to be displayed along with remove card message while a card is inserted and left on screen after the card was removed.
+        /// This message will remain on screen after the card is removed and even if it wasn't present to begin with.
+        /// </summary>
+        public TextProperty<SimpleMessageProperty> RMC_MSG { get; private set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public RmcRequest() 
 		{
 			this.CMD_LEN1 = new RegionProperty("CMD_LEN1", 3);
-			this.RMC_MSG = new SimpleProperty<SimpleMessage>("RMC_MSG", false, DefaultStringFormatter.PropertyControllerStringFormatter, SimpleMessage.StringParser, null, new SimpleMessage());
+			this.RMC_MSG = new TextProperty<SimpleMessageProperty>("RMC_MSG", false, 
+                StringFormatter.PropertyControllerStringFormatter, SimpleMessageProperty.CustomStringParser, null, 
+                new SimpleMessageProperty());
 
 			this.StartRegion(this.CMD_LEN1);
 			{
@@ -21,21 +38,5 @@ namespace Pinpad.Sdk.Commands
 			}
 			this.EndLastRegion();
 		}
-
-		/// <summary>
-		/// Name of the command
-		/// </summary>
-		public override string CommandName { get { return "RMC"; } }
-
-		/// <summary>
-		/// Length of the first region of the command
-		/// </summary>
-		public RegionProperty CMD_LEN1 { get; private set; }
-
-		/// <summary>
-		/// Message to be displayed along with remove card message while a card is inserted and left on screen after the card was removed.
-		/// This message will remain on screen after the card is removed and even if it wasn't present to begin with.
-		/// </summary>
-		public SimpleProperty<SimpleMessage> RMC_MSG { get; private set; }
 	}
 }
